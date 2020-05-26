7813bb19d7da1fa14a34d65851bafaf9c9b30876
Guanya Peng <guanya.peng@roche.com>
2020-05-26 12:06:38 +0200

    update function for generating strums

--- R/Rstrum.R
+++ R/Rstrum.R
@@ -1,42 +1,6 @@
-n <-  8
-n_note <- 3
-n_remove <- n - n_note
-
-make_strum <- function(type, n) replicate(n/2, type)
-
-downs <- make_strum("D", 8)
-ups <- make_strum("U", 8)
-
-make_beats <- function(n) {
-  inner <- function(init, out) {
-    if (init == n) paste(out, '+')
-    else inner(init+1, paste(out, '+', as.character(init+1)))
-  }
-  unlist(str_split(inner(1,'1'), ' '))
-}
-
-all_combine <- function(v, n) {
-  
-}
-
-
-combine_strum <- function(downs, ups) {
-  unlist(str_split(paste(downs, ups), ' '))
-}
-
-gen_patterns <- function(type, n, minn) {
-  n_remove_max <- n - minn
-  
-} 
-out=list()
-mystr <- make_beats(8)
-for (L in combn(c(1:8), 7, simplify = F)) {
-  for (i in L) {
-    mystr[[i]] <- ""
-    out <- c(out, mystr)
-    mystr <- make_beats(8)
-  }
-}
+library("stringr")
+library("tibble")
+library("dplyr")
 
 multi_replace <- function(v, r, ks, split=TRUE, dlm='') {
   if(length(v) == 1 & split) {
@@ -60,7 +24,7 @@ multi_replace <- function(v, r, ks, split=TRUE, dlm='') {
   } else {
     multi_replace(multi_replace(vs, r, ks_i[[1]]), r, ks_i[-1])
   }
-} 
+}
 
 
 
@@ -83,7 +47,7 @@ multi_replace_v <- function(v, r, ks) {
   }
 }
 
-multi_replace_s <- function(s, r, ks) paste0(multi_replace_v(unlist(strsplit(s, '')), r, ks), collapse = '') 
+multi_replace_s <- function(s, r, ks) paste0(multi_replace_v(unlist(strsplit(s, '')), r, ks), collapse = '')
 
 multi_replace <- function(v_or_s, r, ks) {
   if (length(v_or_s) == 1) {
@@ -94,5 +58,50 @@ multi_replace <- function(v_or_s, r, ks) {
     NULL
   }
 }
+
+
+get_all_strums <- function(notetype, n_min, dlm) {
+  make_strum <- function(type, n) replicate(n %/% 2, type)
+
+  make_beats <- function(n) {
+    inner <- function(init, out) {
+      if (init == n %/% 2) paste(out, '+')
+      else inner(init+1, paste(out, '+', as.character(init+1)))
+    }
+    paste(unlist(str_split(inner(1,'1'), ' ')), collapse = '|')
+  }
+
+
+  combine_strum <- function(downs, ups) {
+    unlist(str_split(paste(downs, ups), ' '))
+  }
+
+  get_blank_pos <- function(notetype, n_min) {
+    purrr::flatten(lapply(c(1:(notetype-n_min)), function(n) combn(c(1:notetype), n, simplify = F)))
+  }
+
+  add_blank <- function(L, pos_list) {
+    lapply(pos_list, function(pos) multi_replace(L, " ", pos))
+  }
+
+  blank_pos <- get_blank_pos(notetype, n_min)
+
+  downs <- make_strum("D", notetype)
+  ups <- make_strum("U", notetype)
+
+  strums <- combine_strum(downs, ups)
+  beats <- make_beats(notetype)
+
+  add_dlm <- function(L, dlm) {
+    sapply(L, function(l) paste(l, collapse = dlm))
+  }
+
+  printable_strums <- strums %>% add_blank(blank_pos) %>% add_dlm(dlm)
+  printable_beats <- replicate(length(printable_strums), beats %>% add_dlm(dlm))
+
+  tibble(beat=printable_beats, strum=printable_strums)
+
+  }
+
 
 
---
+++ R/get_all_strums.R
@@ -0,0 +1,42 @@
+get_all_strums <- function(notetype, n_min, dlm) {
+  make_strum <- function(type, n) replicate(n %/% 2, type)
+
+  make_beats <- function(n) {
+    inner <- function(init, out) {
+      if (init == n %/% 2) paste(out, '+')
+      else inner(init+1, paste(out, '+', as.character(init+1)))
+    }
+    paste(unlist(str_split(inner(1,'1'), ' ')), collapse = '|')
+  }
+
+
+  combine_strum <- function(downs, ups) {
+    unlist(str_split(paste(downs, ups), ' '))
+  }
+
+  get_blank_pos <- function(notetype, n_min) {
+    purrr::flatten(lapply(c(1:(notetype-n_min)), function(n) combn(c(1:notetype), n, simplify = F)))
+  }
+
+  add_blank <- function(L, pos_list) {
+    lapply(pos_list, function(pos) multi_replace(L, " ", pos))
+  }
+
+  blank_pos <- get_blank_pos(notetype, n_min)
+
+  downs <- make_strum("D", notetype)
+  ups <- make_strum("U", notetype)
+
+  strums <- combine_strum(downs, ups)
+  beats <- make_beats(notetype)
+
+  add_dlm <- function(L, dlm) {
+    sapply(L, function(l) paste(l, collapse = dlm))
+  }
+
+  printable_strums <- strums %>% add_blank(blank_pos) %>% add_dlm(dlm)
+  printable_beats <- replicate(length(printable_strums), beats %>% add_dlm(dlm))
+
+  tibble(beat=printable_beats, strum=printable_strums)
+
+}
